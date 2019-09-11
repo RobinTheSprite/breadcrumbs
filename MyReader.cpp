ab7de60952f339a4189e22e07968081ff1251c4c
RobinTheSprite <madscientistak@gmail.com>
2019-09-11 13:34:52 -0800

    Prototype functions in MyReader

--- MyReader.cpp
+++ MyReader.cpp
@@ -3,3 +3,18 @@
 //
 
 #include "MyReader.h"
+
+pdal::point_count_t pdal::MyReader::read(PointViewPtr view, point_count_t count)
+{
+    return 0;
+}
+
+void pdal::MyReader::addDimensions(pdal::PointLayoutPtr)
+{
+
+}
+
+void pdal::MyReader::addArgs(pdal::ProgramArgs &)
+{
+
+}
--- MyReader.h
+++ MyReader.h
@@ -5,11 +5,23 @@
 #ifndef PATHFINDER_MYREADER_H
 #define PATHFINDER_MYREADER_H
 
+#include <pdal/PointView.hpp>
+#include <pdal/Reader.hpp>
+#include <pdal/util/IStream.hpp>
 
-class MyReader
+namespace pdal
 {
 
-};
-
+    class MyReader : public pdal::Reader
+    {
+    public:
+        MyReader() : pdal::Reader()
+        {};
+    private:
+        void addDimensions(PointLayoutPtr layoutPtr) override;
+        void addArgs(ProgramArgs &) override;
+        pdal::point_count_t read(PointViewPtr view, point_count_t count) override;
+    };
+}
 
 #endif //PATHFINDER_MYREADER_H
