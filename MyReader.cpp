0ee92acf537010397c757169cc1ebca61e90c1dc
RobinTheSprite <madscientistak@gmail.com>
2019-09-11 13:18:32 -0800

    Create MyReader files

---
+++ MyReader.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Mark on 9/11/2019.
+//
+
+#include "MyReader.h"
---
+++ MyReader.h
@@ -0,0 +1,15 @@
+//
+// Created by Mark on 9/11/2019.
+//
+
+#ifndef PATHFINDER_MYREADER_H
+#define PATHFINDER_MYREADER_H
+
+
+class MyReader
+{
+
+};
+
+
+#endif //PATHFINDER_MYREADER_H
