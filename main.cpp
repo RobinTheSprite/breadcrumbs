f5e9f48ebfd566b06dfcac4094c268c1f054cc5e
RobinTheSprite <madscientistak@gmail.com>
2019-09-11 13:14:11 -0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include <pdal/PointView.hpp>
+#include <pdal/Reader.hpp>
+#include <pdal/util/IStream.hpp>
+
+int main()
+{
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
