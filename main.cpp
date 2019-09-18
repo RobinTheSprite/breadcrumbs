0fbf21259afd26416de5604b914fb80ec66b8033
RobinTheSprite <madscientistak@gmail.com>
2019-09-18 10:42:12 -0800

    Re-orient project. Read coordinates from file.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,37 @@
 #include <iostream>
+#include <pdal/io/LasReader.hpp>
+
+using std::cout;
+using std::endl;
+
+using namespace pdal;
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    LasReader reader;
+
+    Options options;
+//    options.add("filename", "../test_1.laz");
+    options.add("filename", "../test_1.laz");
+    reader.setOptions(options);
+
+    PointTable table;
+    reader.prepare(table);
+
+    std::cout << "Reading LAZ file..." << std::endl;
+    PointViewSet set = reader.execute(table);
+    std::cout << "Finished Reading" << std::endl;
+
+    PointViewPtr view = *set.begin();
+
+    cout << endl << "Data:" << endl;
+    for (unsigned int i = 0; i < view->size(); ++i)
+    {
+        cout << "(";
+        cout << view->getFieldAs<int>(Dimension::Id::X, i) << ", ";
+        cout << view->getFieldAs<int>(Dimension::Id::Y, i) << ", ";
+        cout << view->getFieldAs<int>(Dimension::Id::Z, i) << ")" << endl;
+    }
+
     return 0;
 }
