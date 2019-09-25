e703349304941f94dd8e314133dcb1d349f6987b
RobinTheSprite <madscientistak@gmail.com>
2019-09-24 21:30:30 -0800

    Reproject points into decimal degrees

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <pdal/io/LasReader.hpp>
+#include <pdal/filters/ReprojectionFilter.hpp>
 
 using std::cout;
 using std::endl;
@@ -12,26 +13,41 @@ int main()
 
     Options options;
 //    options.add("filename", "../test_1.laz");
-    options.add("filename", "../test_1.laz");
+    options.add("filename", "../FB17_3764.laz");
     reader.setOptions(options);
 
     PointTable table;
     reader.prepare(table);
 
     std::cout << "Reading LAZ file..." << std::endl;
-    PointViewSet set = reader.execute(table);
+
+    string ddProj4 = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs";
+    SpatialReference newCoordSystem(ddProj4);
+
+    LasHeader header = reader.header();
+
+    cout << "Setting Options" << endl;
+    Options filterOptions;
+    filterOptions.add("in_srs", header.srs());
+    filterOptions.add("out_srs", newCoordSystem.getProj4());
+
+    ReprojectionFilter repro;
+    repro.addOptions(filterOptions);
+    repro.setInput(reader);
+
+    cout << "Executing" << endl;
+    repro.prepare(table);
+    PointViewSet set = repro.execute(table);
+
     std::cout << "Finished Reading" << std::endl;
 
     PointViewPtr view = *set.begin();
 
     cout << endl << "Data:" << endl;
-    for (unsigned int i = 0; i < view->size(); ++i)
-    {
-        cout << "(";
-        cout << view->getFieldAs<int>(Dimension::Id::X, i) << ", ";
-        cout << view->getFieldAs<int>(Dimension::Id::Y, i) << ", ";
-        cout << view->getFieldAs<int>(Dimension::Id::Z, i) << ")" << endl;
-    }
+    cout << "(";
+    cout << view->getFieldAs<double>(Dimension::Id::X, 0) << ", ";
+    cout << view->getFieldAs<double>(Dimension::Id::Y, 0) << ", ";
+    cout << view->getFieldAs<double>(Dimension::Id::Z, 0) << ")" << endl;
 
     return 0;
 }
