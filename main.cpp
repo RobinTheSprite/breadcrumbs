23036fd5faddae49c75a4349d5e44501829038c2
RobinTheSprite <madscientistak@gmail.com>
2020-01-23 23:03:45 -0900

    Implement A* using height differences as movement cost

--- main.cpp
+++ main.cpp
@@ -120,6 +120,120 @@ void writeMatrixToTIFF(vector<vector<float>> matrix)
     _TIFFfree(buf);
 }
 
+void writePathToTIFF(vector<vector<int>> matrix)
+{
+    TIFF * out = TIFFOpen("path.tif", "w");
+
+    int width = matrix[0].size();
+    int height = matrix.size();
+    int samplesPerPixel = 1;
+
+    TIFFSetField(out, TIFFTAG_IMAGEWIDTH, width);
+    TIFFSetField(out, TIFFTAG_IMAGELENGTH, height);
+    TIFFSetField(out, TIFFTAG_SAMPLESPERPIXEL, samplesPerPixel);
+    TIFFSetField(out, TIFFTAG_BITSPERSAMPLE, 32);
+    TIFFSetField(out, TIFFTAG_ORIENTATION, ORIENTATION_TOPLEFT);
+    TIFFSetField(out, TIFFTAG_PLANARCONFIG, PLANARCONFIG_CONTIG);
+    TIFFSetField(out, TIFFTAG_PHOTOMETRIC, PHOTOMETRIC_MINISBLACK);
+    TIFFSetField(out, TIFFTAG_SAMPLEFORMAT, SAMPLEFORMAT_INT);
+
+    size_t bytesPerLine = width * sizeof(int);
+    int * buf = nullptr;
+
+    if (TIFFScanlineSize(out))
+    {
+        buf = (int *)_TIFFmalloc(bytesPerLine);
+    }
+    else
+    {
+        buf = (int *)_TIFFmalloc(TIFFScanlineSize(out));
+    }
+
+    TIFFSetField(out, TIFFTAG_ROWSPERSTRIP, 1);
+
+    for (auto i = 0ul; i < matrix.size(); ++i)
+    {
+        memcpy(buf, matrix[i].data(), bytesPerLine);
+        if (TIFFWriteScanline(out, buf, i, 0) < 0)
+        {
+            cout << "Error Writing TIFF" << endl;
+            break;
+        }
+    }
+
+    TIFFClose(out);
+    _TIFFfree(buf);
+}
+
+struct MatrixPoint
+{
+    int x;
+    int y;
+    float movementCost;
+    float heuristic;
+    float totalCost;
+};
+
+vector<vector<int>> getShortestPath(const vector<vector<float>> & matrix, MatrixPoint startingPoint, MatrixPoint target)
+{
+    auto differenceGreater = [](auto a, auto b){return a.totalCost > b.totalCost;};
+    using PointQueue = priority_queue<MatrixPoint, vector<MatrixPoint>, decltype(differenceGreater)>;
+    PointQueue openSet(differenceGreater);
+
+    startingPoint.heuristic = 0;
+    startingPoint.movementCost = 0;
+    startingPoint.totalCost = 0;
+    openSet.push(startingPoint);
+
+    auto pathMatrix = vector<vector<int>>(matrix.size(), vector<int>(matrix[0].size(), 0));
+    auto visitedPoint = 100;
+
+    vector<MatrixPoint> surroundingPoints(4);
+
+    bool stop = false;
+    while (!stop)
+    {
+        auto currentPoint = openSet.top();
+        openSet.pop();
+
+        surroundingPoints = {{currentPoint.x + 1, currentPoint.y},
+                             {currentPoint.x - 1, currentPoint.y},
+                             {currentPoint.x,     currentPoint.y + 1},
+                             {currentPoint.x,     currentPoint.y - 1},
+                             {currentPoint.x + 1, currentPoint.y + 1},
+                             {currentPoint.x - 1, currentPoint.y - 1},
+                             {currentPoint.x - 1, currentPoint.y + 1},
+                             {currentPoint.x + 1, currentPoint.y - 1}
+        };
+
+        auto inBounds = [matrix](auto n) { return n > -1 && n < matrix.size() && n < matrix[0].size(); };
+        for (const auto &point : surroundingPoints)
+        {
+            if (point.x == target.x && point.y == target.y)
+            {
+                pathMatrix[point.y][point.x] = visitedPoint;
+                stop = true;
+                break;
+            }
+
+            if (inBounds(point.x) && inBounds(point.y))
+            {
+                if (pathMatrix[point.y][point.x] == 0)
+                {
+                    auto difference = abs(matrix[point.y][point.x] - matrix[currentPoint.y][currentPoint.x]);
+                    auto movementCost = difference + point.movementCost;
+                    float heuristic = std::max(abs(target.x - currentPoint.x), abs(target.y - currentPoint.y));
+                    openSet.push({point.x, point.y, movementCost, heuristic, movementCost + heuristic});
+                }
+            }
+        }
+
+        pathMatrix[currentPoint.y][currentPoint.x] = visitedPoint;
+    }
+
+    return pathMatrix;
+}
+
 int main()
 {
 //    auto cloud1 = getTIFF("../bh_FB17_3764.tif");
@@ -136,16 +250,9 @@ int main()
 
     cout << "Columns: " << matrix[0].size() << endl;
 
-    for (auto row : matrix)
-    {
-        if (!std::all_of(row.begin(), row.end(), [](auto f){return f > 500 && f < 1500;}))
-        {
-            cout << "Matrix Broken!" << endl;
-            cout << *std::find_if(row.begin(), row.end(), [](auto f){return f < 500 || f > 1500;}) << endl;
-        }
-    }
+    auto pathMatrix = getShortestPath(matrix, {0, 0}, {200, 100});
 
-    writeMatrixToTIFF(matrix);
+    writePathToTIFF(pathMatrix);
 
     return 0;
 }
