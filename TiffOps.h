0c926affb32977105fff3487185456dc47d2a79c
Mark Underwood <mark.t.underwood@protonmail.com>
2020-12-27 17:51:46 -0900

    Document functions in TiffOps.h

--- TiffOps.h
+++ TiffOps.h
@@ -8,10 +8,24 @@
 #include <vector>
 #include <string>
 
+/*
+ * Reads a TIFF into a 2D vector of floats.
+ * The TIFF must be stored in tiles, and contain 32-bit floating point numbers.
+ */
 std::vector<std::vector<float>> readTIFF(const std::string & filename);
 
+/*
+ * Writes a 2D matrix of floats to a TIFF.
+ * No spatial reference is written.
+ * The units per pixel is also not recorded.
+ */
 void writeMatrixToTIFF(std::vector<std::vector<float>> matrix, const std::string & filename);
 
+/*
+ * Writes a 2D matrix of ints to a TIFF.
+ * No spatial reference is written.
+ * The units per pixel is also not recorded.
+ */
 void writePathToTIFF(std::vector<std::vector<int>> matrix, std::string filename);
 
 #endif //BREADCRUMBS_TIFFOPS_H
